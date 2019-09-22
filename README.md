8995d71e6f99765f368d325ebcff293ea6d62e70
Manu Varma <varmam@bxscience.edu>
2019-09-22 14:53:13 -0400

    Start of creating server from tutorial

---
+++ README.md
@@ -0,0 +1,10 @@
+Trying to learn how to use websockets in Golang for practice
+with Golang and Websockets. 
+
+Goals:
+    Create a simple chat app
+    Create chatrooms
+    Create a decent frontend
+    Be able to create private and public chatrooms
+
+Initial stuff followed from https://medium.com/@johnshenk77/create-a-simple-chat-application-in-go-using-websocket-d2cb387db836
---
+++ server/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"golang.org/x/net/websocket"
+)
+
+// JSON serializable struct to hold messages
+type Message struct{
+	Text string `json:"text"`
+}
+
+// Holds clients in a map and the channels
+type hub struct{
+	clients          map[string]*websocket.Conn
+	addClientChan    chan *websocket.Conn
+	removeClientChan chan *websocket.Conn
+	broadcastChan    chan Message   
+}
+
+var(
+	port = flag.String("port", "9000", "port used for ws connection")
+)
+
+func server(port string) error{
+	h := newHub()
+	mux := http.NewServeMux()
+	// Routes / to handler
+	mux.Handle("/", websocket.Handler(func(ws *websocket.Conn) {
+		handler(ws, h)
+	}))
+
+	s := http.Server{Addr: ":" + port, Handler:mux}
+	return s.ListenAndServe()
+}
+
+// Every client that connects will call this handler
+// Runs the hub and then listens for messages from the client
+func handler(ws *websocket.Conn, h *hub){
+	go h.run()
+
+	h.addClientChan <- ws
+
+	// TODO: Improve error handling
+	for {
+		var m Message
+		err := websocket.JSON.Recieve(ws, &m)
+		if err != nil {
+			h.broadcastChan <- Message{err.Error()}
+			h.removeClient(ws)
+			return
+		}
+		h.broadcastChan <- m
+	}
+}
+
+// Returns a new hub
+func newHub() *hub{
+	return &hub{
+		clients:          make(map[string]*websocket.Conn),
+		addClientChan:    make(chan *websocket.Conn),
+		removeClientChan: make(chan *websocket.Conn),
+		broadcastChan:    make(chan Message),	  
+	}
+}
